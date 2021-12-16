8a218709db63ea192284476f2c80bed629ed810c
vanditechnician@gmail.com <vanditechnician@gmail.com>
2021-12-16 07:42:45 -0600

    Created a Array of 5 objects which hold 2 keys: A Quote string, and a Source String

--- js/script.js
+++ js/script.js
@@ -4,13 +4,31 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-
+//Created a Array of 5 objects which hold 2 keys: A Quote string, and a Source String
+let quotes = [
+  {
+    quote: 'Getting information off the internet is like taking a drink from a fire hydrant.',
+    source: '-Mitch Kapor'
+  },
+  {
+    quote: 'Cyberspace can\'t compensate for real space. We benefit from chatting to people face to face',
+    source: '-Jonathan Sacks'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing',
+    source: '-Walt Disney'
+  },
+  {
+    quote: 'If you set your goals ridiculously high and it\'s a failure, you will fail above everyone else\'s success.',
+    source: '-James Cameron'
+  },
+  {
+    quote: 'Life is what happens when you\'re busy making other plans.',
+    source: '-John Lennon'
+  }];
 
 
 /***
